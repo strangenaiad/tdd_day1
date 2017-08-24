8ec0d51ab49c80e1ce01b0c319c7ccd8e8ff9c94
strangenaiad <strange.naiad@gmail.com>
2017-08-24 17:04:11 +0800

    1st time commit

---
+++ UserProfit/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserProfit
+{
+    public class UserProfitDaily
+    {
+        internal int Sum( int[]arr )
+        {
+            int sum = 0;
+            for (var i = 0; i < arr.Length; i++)
+                sum += arr[i];
+
+            return sum;
+        }
+
+        public int[] Summary( List<int>all, int period )
+        {
+            List<int> ret = new List<int>();
+
+            var i = 0;
+            while (i < all.Count())
+            {
+                var num = all.Count() - i;
+                if (num > period)
+                    num = period;
+
+                int[] arr = new int[num];
+                all.CopyTo(i, arr, 0, num);
+                ret.Add(Sum(arr));
+
+                i += num;
+            }
+
+            return ret.ToArray();
+        }
+    }
+}
---
+++ UserProfitTests/UserProfitDailyTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserProfit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserProfit.Tests
+{
+    [TestClass()]
+    public class UserProfitDailyTests
+    {
+        [TestMethod()]
+        public void SumTest_cost_with_3_period()
+        {
+            List<int> Cost = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            var target = new UserProfitDaily();
+
+            int[] expect = { 6, 15, 24, 21 };
+            int[] actual = target.Summary(Cost, 3);
+
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void SummaryTest_cost_with_3_period()
+        {
+            List<int> Revenue = new List<int> { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 };
+            var target = new UserProfitDaily();
+
+            int[] expect = { 50, 66, 60 };
+            int[] actual = target.Summary(Revenue, 4);
+
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+    }
+}
